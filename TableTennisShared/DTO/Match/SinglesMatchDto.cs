414a5558e16e31f4e09e0ab10468e8dfe1661337
RickSiep <rick-siepelinga@hotmail.com>
2026-05-20 15:40:34 +0200

    start suggestionInput

--- TableTennisShared/DTO/Match/SinglesMatchDto.cs
+++ TableTennisShared/DTO/Match/SinglesMatchDto.cs
@@ -2,5 +2,9 @@
 {
     public class SinglesMatchDto
     {
+        public int WinnerId { get; set; }
+        public int LoserId { get; set; }
+        public int WinnerScore { get; set; }
+        public int LoserScore { get; set; }
     }
 }
--- tabletennisAPI/Util/EloCalculator.cs
+++ tabletennisAPI/Util/EloCalculator.cs
@@ -4,8 +4,8 @@
     {
         public static (double winnerElo, double loserElo) CalculateEloForSinglesGame(double winnerCurrentElo, double loserCurrentElo)
         {
-            double playerAChanceOfWinning = 1.00 / (1.00 + (Math.Pow(10.00, (loserCurrentElo - winnerCurrentElo) / 400)));
-            double playerBChanceOfWinning = 1 - playerAChanceOfWinning;
+            var playerAChanceOfWinning = 1.00 / (1.00 + (Math.Pow(10.00, (loserCurrentElo - winnerCurrentElo) / 400)));
+            var playerBChanceOfWinning = 1 - playerAChanceOfWinning;
 
             var newRatingPlayerA = winnerCurrentElo + 32 * (1 - playerAChanceOfWinning);
             var newRatingPlayerB = loserCurrentElo + 32 * (0 - playerBChanceOfWinning);
